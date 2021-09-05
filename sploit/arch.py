2d14b4f5cb7a10ead41cd6e956050a5ecef81a20
Malfurious <m@lfurio.us>
2021-09-04 20:53:00 -0400

    sploit: Rename arch.nop to nopcode
    This was the name I had originally intended to use while factoring
    architecture details out to the global scope.  It's not terribly
    different, but I feel the new context warrants some additional clarity.
    
    Signed-off-by: Malfurious <m@lfurio.us>
    Signed-off-by: dusoleil <howcansocksbereal@gmail.com>

--- sploit/arch.py
+++ sploit/arch.py
@@ -5,24 +5,24 @@ def itob(i, signed=False):
     return i.to_bytes(arch.wordsize, arch.endianness, signed=signed)
 
 class Arch:
-    def __init__(self, wordsize, endianness, alignment, nop):
+    def __init__(self, wordsize, endianness, alignment, nopcode):
         self.wordsize = wordsize
         self.endianness = endianness
         self.alignment = alignment
-        self.nop = nop
+        self.nopcode = nopcode
 
 archx86 = Arch(
     wordsize = 4,
     endianness = "little",
     alignment = 16,
-    nop = b'\x90'
+    nopcode = b'\x90'
 )
 
 archx86_64 = Arch(
     wordsize = 8,
     endianness = "little",
     alignment = 16,
-    nop = b'\x90'
+    nopcode = b'\x90'
 )
 
 arch = archx86_64
